ed6defe886f86022639d3b6634b0e9636c667fc5
Rodrigo Verruck Pedro <rodrigoverruck@gmail.com>
2023-04-21 16:41:12 -0300

    editei o programa

--- teste/src/PrimeiroPrograma.java
+++ teste/src/PrimeiroPrograma.java
@@ -1,5 +1,5 @@
 public class PrimeiroPrograma {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        System.out.println("Olá, mundo");
     }
 }
